a7e4e269766bd340d9760997f798c2054aec7291
Courtney G <ckg35@cornell.edu>
2022-11-02 16:32:05 -0400

    One python script for all jsons

--- request_video_analytics_concurr.py
+++ request_video_analytics_concurr.py
@@ -12,42 +12,49 @@ location = "delhi technological university"
 PARAMS = {'address':location}
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS)
+# r = requests.get(url = URL, params = PARAMS)
 
 # extracting data in json format
-data = r.json()
+# data = r.json()
 
-output = open("output_va_concurr_3frames_2replica_medium.txt", "a")
-output.write("URL: " + URL + "\n")
+output = open("output_va_concurr_20frames_2replica.txt", "a")
+recog_diff = []
 
-filtered = list(filter(lambda d: 'videoservice.videodecoder/decode' in d["name"], data))
-if (len(filtered) != 0):
-    print(len(filtered))
-    streaming_timestamp = filtered[0]['timestamp']
-    print(streaming_timestamp)
-    decode_timestamp = filtered[1]['timestamp']
-    print(decode_timestamp)
-    decode_diff = decode_timestamp - streaming_timestamp
-    print(decode_diff)
-    output.write("" + str(streaming_timestamp) + "\n")
-    output.write("" + str(decode_timestamp) + "\n")
-    output.write("Decode_diff: " + str(decode_diff) + "\n")
+for i in range(1, len(sys.argv)):
+    URL = sys.argv[i]
+    r = requests.get(url = URL, params = PARAMS)
+    data = r.json()
+    output.write("URL: " + URL + "\n")
 
-filtered = list(filter(lambda d: d["name"] == 'helloworld.greeter/sayhello', data))
-if (len(filtered) != 0):
-    duration = filtered[0]['duration']
-    output.write("Duration: " + str(duration) + "\n")
+    filtered = list(filter(lambda d: 'videoservice.videodecoder/decode' in d["name"], data))
+    if (len(filtered) != 0):
+        # print(len(filtered))
+        streaming_timestamp = filtered[0]['timestamp']
+        # print(streaming_timestamp)
+        decode_timestamp = filtered[1]['timestamp']
+        # print(decode_timestamp)
+        decode_diff = decode_timestamp - streaming_timestamp
+        # print(decode_diff)
+        output.write("" + str(streaming_timestamp) + "\n")
+        output.write("" + str(decode_timestamp) + "\n")
+        output.write("Decode_diff: " + str(decode_diff) + "\n")
 
-recog_times = []
-filtered = list(filter(lambda d: d["name"] == '/videoservice.objectrecognition/recognise', data))
-if (len(filtered) != 0):
-    print(len(filtered))
-    recog_times = [elem['timestamp'] for elem in filtered]
-    recog_times.sort()
+    filtered = list(filter(lambda d: d["name"] == 'helloworld.greeter/sayhello', data))
+    if (len(filtered) != 0):
+        duration = filtered[0]['duration']
+        output.write("Duration: " + str(duration) + "\n")
 
-    recog_diff = recog_times[1] - recog_times[0]
+    recog_times = []
+    filtered = list(filter(lambda d: d["name"] == '/videoservice.objectrecognition/recognise', data))
+    if (len(filtered) != 0):
+        recog_times = [elem['timestamp'] for elem in filtered]
+        recog_times.sort() 
+        local_diff = recog_times[1] - recog_times[0]
+        print("local diff: " + str(local_diff))
+        recog_diff.append(local_diff)
 
-    output.write("" + str(recog_times) + "\n")
-    output.write("Recog_diff: " + str(recog_diff) + "\n")
+# Once done, calculate max recognition time
+max_recog = max(recog_diff, default=0)
+output.write("Max Recog Diff: " + str(max_recog) + "\n")
 
 output.close()
